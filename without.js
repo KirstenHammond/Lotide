04db53595a8222c96bc0df5c23ad72b786fbb723
Kirsten Hammond <113560819+KirstenHammond@users.noreply.github.com>
2022-11-16 14:06:32 -0800

    refactored to use ternary and updated eq funcs

--- without.js
+++ without.js
@@ -1,32 +1,40 @@
 
-const eqArrays = function(a, b) {
-  //console.log(a);// checking
-  // console.log(a.length);//checking
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] !== b[i]) {
-      return false; // by entering into this if, it will return false and stop the execution
+const eqArrays = function (array1, array2) {
+  for (let i of array1.keys()) {
+    if (array1[i] !== array2[i]) {
+      return false;
     }
-  } return true; //otherwise if it doesnt enter into the conditional, it'll return true
+  } return true;
 };
 
-const assertArraysEqual = function(a, b) {
-  let bool = eqArrays(a, b);
-  return bool ? `🟢🟢🟢Assertion Passed` : `🟥🟥🟥Assertion Failed`;
+const assertArraysEqual = function (array1, array2) {
+  let isEqual = eqArrays(array1, array2);
+  return isEqual ? `🟢🟢🟢Assertion Passed` : `🟥🟥🟥Assertion Failed`;
 };
 
 
-const without = function(source, itemsToRemove) {
+/* const without = function (source, itemsToRemove) {
   let newArr = [];
   for (i of source) {
-  //console.log(i); checking
+    //console.log("i = ", i); checking
     if (!itemsToRemove.includes(i)) { //if i is not in the itemsToRemove array
-    //console.log(i);//checking
-      newArr.push(i); //then push that value to a new array
+      //console.log("i values leftover=", i);//checking
+      newArr.push(i);
     }
-  } console.log(assertArraysEqual(source, ["4", "7", "1"]));// testing?? Don't think this is supposed to be within the function? Instructions are unclear.
-  return newArr;
+  } return newArr;
+}; 
+
+Refactored to use ternary below :*/
+
+
+const without = function (source, itemsToRemove) {
+  let newArr = [];
+  for (i of source) {
+    !itemsToRemove.includes(i) ? newArr.push(i) : null;
+   } return newArr;
 };
 
-console.log(without(["4", "7", "1"], ["1", "4"]));
 
-
+//Test Code
+console.log(without(["4", 7, 1], ["4"]));
+console.log(assertArraysEqual(without(["4", 7, 1], ["4"]), [7, 1]));// testing?? Don't think this is supposed to be within the function? Instructions are unclear.
