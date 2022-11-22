0cb8d9736a044037412ed2ad76227f32bda4d60c
Kirsten Hammond <113560819+KirstenHammond@users.noreply.github.com>
2022-11-21 18:22:02 -0800

    added descriptor

--- findKey.js
+++ findKey.js
@@ -1,5 +1,5 @@
 //Find Key
-
+//takes in an object and a callback. It should scan the object and return the first key for which the callback returns a truthy value. If no key is found, then it should return undefined
 const findKey = function(object, callback) {
   //let keyList = Object.keys(object);
   //console.log(object["Blue Hill"]);
